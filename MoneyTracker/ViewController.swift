174c43b0d1e6cdc7ba79639625390bf73b5d8ece
Anton Chernyshev <ddropwnz@gmail.com>
2022-02-12 16:47:19 +0300

    Initial commit

---
+++ MoneyTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneyTracker
+//
+//  Created by ddrop on 12.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
